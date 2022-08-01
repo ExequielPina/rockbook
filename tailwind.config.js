0eae52188b953c18f7cf78192731056f5c53deef
Exequiel Piña <38662692+ExequielPina@users.noreply.github.com>
2022-08-01 12:31:06 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./resources/**/*.blade.php", "./resources/**/*.js", "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
